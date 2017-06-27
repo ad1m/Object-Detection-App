ddb168cb79dd0dda1a81a6da7b2ef17fa6c45615
ad1m <alieberman3@mail.gatech.edu>
2017-06-27 11:14:39 -0400

    Changed Heroku port

--- app.py
+++ app.py
@@ -47,4 +47,6 @@ def index2():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True,port=5000)
